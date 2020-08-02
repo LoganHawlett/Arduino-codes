5aa48a7ab4cf4682ca8344d4fcb0ead6a0b49d6f
LoganHawlett <69051203+LoganHawlett@users.noreply.github.com>
2020-08-02 19:54:01 +0530

    Add files via upload

---
+++ Tsop_function/Tsop_function.ino
@@ -0,0 +1,42 @@
+/*
+ * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+ * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
+
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+int LED=9;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+  if (results.value==0x1FEE01F)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  if(results.value==0x1FE50AF)
+  {
+    digitalWrite(LED,LOW);
+  }
+}
